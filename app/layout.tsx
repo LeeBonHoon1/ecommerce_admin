3895323312967f28fd618d3d594079a8556d8d5f
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2023-09-20 16:21:57 +0900

    store prismadb connect

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-const inter = Inter({ subsets: ["latin"] });
 
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Admin Dashboard",
   description: "Admin Dashboard",
@@ -17,7 +21,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
