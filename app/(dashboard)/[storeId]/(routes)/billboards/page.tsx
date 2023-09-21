50d381d08bddf0ae0b9cb10abd02760f2b12fe1b
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2023-09-21 18:18:30 +0900

    cloudinary image upload

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,12 +1,37 @@
+"use client";
+
+import { Heading } from "@/components/ui/heading";
 import BillboardClient from "./components/client";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
 
 const BillBoardPage = () => {
+  const router = useRouter();
+  const params = useParams();
+
   return (
-    <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+    <>
+      <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <Heading
+            title="Billboards (0)"
+            description="Manage billboards for your store"
+          />
+          <Button
+            onClick={() => {
+              router.push(`/${params.storeId}/billboards/new`);
+            }}
+          >
+            <Plus className="mr-2 h-4 w-4" />
+            Add New
+          </Button>
+          <BillboardClient />
+        </div>
       </div>
-    </div>
+      <Separator />
+    </>
   );
 };
 
